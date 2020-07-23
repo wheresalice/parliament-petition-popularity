deb9e84b43fe03ee63b6d51372464e8a471bf884
Alice <WheresAlice@users.noreply.github.com>
2020-07-23 19:11:40 +0100

    clean up

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ st.title('Petition signature counts')
 petition_1 = st.text_input("petition id", "331430")
 petition_2 = st.text_input("petition id", "325327")
 
-
-data_load_state = st.text('Loading data...')
-
-
-
 petition_1_data = requests.get(f"https://petition.parliament.uk/petitions/{petition_1}.json").json()
 petition_2_data = requests.get(f"https://petition.parliament.uk/petitions/{petition_2}.json").json()
 
